9043fa7e7bf3fd1ed9cea1a46b6482c42a844adb
Manish <manish434k@yahoo.com>
2024-05-17 23:54:04 +0200

    updated code

--- rokda-app/src/App.tsx
+++ rokda-app/src/App.tsx
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './Style';
 import Table from './Components/Table';
 
+
+
+
+
 const headers = ['Sq.', 'Name', 'Symbol', 'Quantity', 'Net Cost', 'Current net value', 'Avg. buy price', 'Market price', 'Unrealised PnL', 'Unrealised PnL percent' ];
 const data = [
     ['1', 'ITC Ltd', 'ITC', '30', '100000', '150000', '220', '430', '50000', '50%'],
@@ -22,18 +26,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p> */}
-        <a
-          // className="App-link"
-          // href="https://reactjs.org"
-          // target="_blank"
-          // rel="noopener noreferrer"
-        >
-        ROKDA
-        </a>
+        <a className='App-logo'> Rokda </a>
       </header>
       <div className='App-body'>
         <Table headers={headers} data={data}></Table>
--- rokda-app/src/Style.tsx
+++ rokda-app/src/Style.tsx
@@ -1,5 +1,5 @@
 
 // Import all style css in this file 
-
-import './App.css';
+import './style/index.css';
+import './style/App.css';
 import './style/Table.css'
